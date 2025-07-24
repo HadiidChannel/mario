041fb6121ec62c115d3953303af46ee3ddf9edd0
hadiidarraad <hadiidradox@gmail.com>
2025-07-24 15:33:57 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,80 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 500 },
+      debug: false
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+
+const game = new Phaser.Game(config);
+let player, cursors, platforms;
+
+function preload() {
+  this.load.image('tiles', 'assets/tileset.png');
+  this.load.tilemapTiledJSON('map', 'assets/map.json');
+  this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 32 });
+}
+
+function create() {
+  const map = this.make.tilemap({ key: 'map' });
+  const tileset = map.addTilesetImage('tileset', 'tiles');
+  const layer = map.createLayer('Platform', tileset, 0, 0);
+  layer.setCollisionByProperty({ collides: true });
+
+  platforms = layer;
+
+  player = this.physics.add.sprite(100, 450, 'player');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'player', frame: 4 } ],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.physics.add.collider(player, layer);
+
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.blocked.down) {
+    player.setVelocityY(-330);
+  }
+}
